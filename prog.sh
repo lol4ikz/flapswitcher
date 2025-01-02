452a884af613f076a48754f59489c17299e3e19e
lol4ikz <nurmukhanov.k@gmail.com>
2025-01-02 10:42:25 +0500

    added CMake

--- prog.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-avr-gcc -g -Os -mmcu=attiny13a -c attiny13.c
-avr-gcc -g -mmcu=attiny13a -o attiny13.elf attiny13.o
-avr-objcopy -j .text -j .data -O ihex attiny13.elf attiny13.hex
-avr-size --format=avr --mcu=attiny13a attiny13.elf
